5624f7fe4a954acf0846234d9e2dcdade50e48aa
Crowdbotics <team@crowdbotics.com>
2022-04-21 07:04:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "high_angle_34857",
+  url: "https://high_angle_34857.botics.co",
+  api: "https://high_angle_34857.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
